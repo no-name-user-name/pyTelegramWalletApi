0e709161528191db4793aedfd4e9455f5e690635
NO NAME <dimazver161@gmail.com>
2023-10-14 05:58:24 +0300

    v0.2.1

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
-
-with open("README.md", "r") as readme_file:
-    readme = readme_file.read()
+from pathlib import Path
 
 requirements = ["requests", "selenium-wire", "pytest"]
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='pyTelegramWalletApi',
@@ -13,4 +13,6 @@ setup(
     packages=find_packages(),
     install_requires=requirements,
     author_email='dimazver61@gmail.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
